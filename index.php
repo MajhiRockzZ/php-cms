bd9a4965259be0f9f7ad23157a536bd31f2caed6
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-02-04 06:59:59 +0530

    feat: Make navigation reusable

--- index.php
+++ index.php
@@ -1,40 +1,8 @@
-<?php
-
-include "includes/header.php"
-
-?>
+<?php include "includes/header.php" ?>
 
 <!-- Navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Start Bootstrap</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-</nav>
+<?php include "includes/navigation.php" ?>
+
 
 <!-- Page Content -->
 <div class="container">
@@ -172,3 +140,5 @@ include "includes/header.php"
     <!-- /.row -->
 
     <hr>
+
+    <?php include "includes/footer.php" ?>
