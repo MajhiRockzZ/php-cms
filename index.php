1652dcaa401d039fc1c13a28232467cf8a2a757b
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-02-07 07:39:13 +0530

    feat: Inserting Data into the posts table and displaying it

--- index.php
+++ index.php
@@ -12,6 +12,16 @@
 
         <!-- Blog Entries Column -->
         <div class="col-md-8">
+            <?php 
+            $query = "SELECT * FROM posts";
+            $select_all_posts_query = mysqli_query($connection, $query);
+            
+            while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                $post_title = $row['post_title'];
+                $post_author = $row['post_author'];
+                $post_date = $row['post_date'];
+                $post_image = $row['post_image'];
+                $post_content = $row['post_content'] ?>
 
             <h1 class="page-header">
                 Page Heading
@@ -20,20 +30,28 @@
 
             <!-- First Blog Post -->
             <h2>
-                <a href="#">Blog Post Title</a>
+                <a href="#">
+                    <?php echo $post_title ?></a>
             </h2>
             <p class="lead">
-                by <a href="index.php">Start Bootstrap</a>
+                by <a href="index.php">
+                    <?php echo $post_author ?></a>
             </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+            <p><span class="glyphicon glyphicon-time"></span>
+                <?php echo $post_date?>
+            </p>
             <hr>
             <img class="img-responsive" src="http://placehold.it/900x300" alt="">
             <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+            <p>
+                <?php echo $post_content ?>
+            </p>
             <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
             <hr>
 
+
+            <?php } ?>
         </div>
 
         <!-- Blog Sidebar Widgets Column -->
