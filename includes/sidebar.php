71ec89c3b3afa1186d341351da62b99c940aad7f
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-03-03 14:55:39 +0530

    feat: Adding Categories to the sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -5,56 +5,41 @@
         <h4>Blog Search</h4>
         <form action="search.php" method="post">
             <div class="input-group">
-                <input name="search" type="text" class="form-control">
                 <span class="input-group-btn">
+                <input name="search" type="text" class="form-control">
                     <button name="submit" class="btn btn-default" type="submit">
                         <span class="glyphicon glyphicon-search"></span>
                     </button>
                 </span>
             </div>
         </form> <!-- Search Form -->
-        <!-- /.input-group -->
-    </div>
+        <!-- /.input-group -->    </div>
+
 
     <!-- Blog Categories Well -->
     <div class="well">
+        <?php
+        $query = "SELECT * FROM categories LIMIT 3";
+        $select_categories_sidebar = mysqli_query($connection, $query);
+        ?>
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                    <?php
+                    while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                        $cat_title = $row['cat_title'];
+
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                    }
+                    ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus
-            laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include "widget.php" ?>
 
 </div>
