d3e685b74225cad9da54fb5153ab22d0659fde37
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-03-03 12:13:16 +0530

    feat: Creating a custom search engine part 2

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,7 +2,23 @@
 
     <?php
     if(isset($_POST['submit'])) {
-       echo $search = $_POST['search'];
+       $search = $_POST['search'];
+        
+        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+        $search_query = mysqli_query($connection, $query);
+        
+        if(!$search_query) {
+            // Checking for query and printing error
+            die("QUERY FAILED". mysqli_error($connection));
+        }
+        
+        $count = mysqli_num_rows($search_query);
+        
+        if($count == 0) {
+            echo "<h1> NO RESULTS </h1>";
+        } else {
+            echo "SOME RESULT";
+        }
     }
     ?>
 
