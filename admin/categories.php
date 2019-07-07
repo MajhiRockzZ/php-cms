0e29010df11c60ba6db1e059006842cb17e4c1eb
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-07 09:37:16 +0530

    feat: Deleting Categories

--- admin/categories.php
+++ admin/categories.php
@@ -49,10 +49,6 @@
           </div> <!-- Add Category Form -->
 
           <div class="col-xs-6">
-            <?php
-            $query = "SELECT * FROM categories";
-            $select_categories = mysqli_query($connection, $query);
-            ?>
             <table class="table table-bordered table-hover">
               <thead>
                 <tr>
@@ -63,6 +59,10 @@
               <tbody>
 
                 <?php
+                //FIND ALL CATEGORIES QUERY
+                $query = "SELECT * FROM categories";
+                $select_categories = mysqli_query($connection, $query);
+
                 while ($row = mysqli_fetch_assoc($select_categories)) {
                   $cat_id = $row['cat_id'];
                   $cat_title = $row['cat_title'];
@@ -70,7 +70,18 @@
                   echo "<tr>";
                   echo "<td>{$cat_id}</td>";
                   echo "<td>{$cat_title}</td>";
+                  echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
                   echo "</tr>";
+                }
+                ?>
+
+                <?php
+                // DELETE QUERY
+                if (isset($_GET['delete'])) {
+                  $the_cat_id = $_GET['delete'];
+                  $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+                  $delete_query = mysqli_query($connection, $query);
+                  header("Location: categories.php");
                 }
                 ?>
               </tbody>
