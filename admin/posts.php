b11753079baae98e343b6bb1216d117772731919
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-09 13:55:29 +0530

    feat: Including Pages based on condition technique

--- admin/posts.php
+++ admin/posts.php
@@ -18,64 +18,32 @@
             <small>Author</small>
           </h1>
 
-          <table class="table table-bordered table-hover">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Author</th>
-                <th>Title</th>
-                <th>Category</th>
-                <th>Status</th>
-                <th>Image</th>
-                <th>Tags</th>
-                <th>Comments</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody>
+          <?php
+          if (isset($_GET['source'])) {
+            $source = $_GET['source'];
+          } else {
+            $source = '';
+          }
 
-              <?php
-              $query = "SELECT * FROM posts";
-              $select_posts = mysqli_query($connection, $query);
+          switch ($source) {
+            case '34';
+              echo "NICE 34";
+              break;
 
-              while ($row = mysqli_fetch_assoc($select_posts)) {
-                $post_id = $row['post_id'];
-                $post_author = $row['post_author'];
-                $post_title = $row['post_title'];
-                $post_category_id = $row['post_category_id'];
-                $post_status = $row['post_status'];
-                $post_image = $row['post_image'];
-                $post_tags = $row['post_tags'];
-                $post_comment_count = $row['post_comment_count'];
-                $post_date = $row['post_date'];
+            case '100';
+              echo "NICE 100";
+              break;
 
-                echo "<tr>";
-                echo "<td>$post_id</td>";
-                echo "<td>$post_author</td>";
-                echo "<td>$post_title</td>";
-                echo "<td>$post_category_id</td>";
-                echo "<td>$post_status</td>";
-                echo "<td><img class='img-responsive' width='100' src='../images/$post_image'></td>";
-                echo "<td>$post_tags</td>";
-                echo "<td>$post_comment_count</td>";
-                echo "<td>$post_date</td>";
-                echo "</tr>";
-              }
-              ?>
+            case '200';
+              echo "NICE 200";
+              break;
 
-              <tr>
-                <td>1</td>
-                <td>Sumesh Majhi</td>
-                <td>Bootstrap</td>
-                <td>UI Framework</td>
-                <td>Public</td>
-                <td>Image</td>
-                <td>Tags</td>
-                <td>Comment</td>
-                <td>Date</td>
-              </tr>
-            </tbody>
-          </table>
+            default:
+              include "includes/view_all_posts.php";
+              break;
+          }
+          ?>
+
         </div>
       </div>
       <!-- /.row -->
