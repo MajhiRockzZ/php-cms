7aaa2d90c093cb3f2e73967b8f5f3d0c695e67ba
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-09 13:19:55 +0530

    feat: Creating a HTML table in admin to display a list of posts

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -37,7 +37,7 @@
         <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
         <ul id="posts_dropdown" class="collapse">
           <li>
-            <a href="#">View All Posts</a>
+            <a href="posts.php">View All Posts</a>
           </li>
           <li>
             <a href="#">Add Posts</a>
@@ -45,7 +45,7 @@
         </ul>
       </li>
       <li>
-        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+        <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
       </li>
       <li class="active">
         <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
---
+++ admin/posts.php
@@ -0,0 +1,58 @@
+<?php include "includes/admin_header.php" ?>
+
+<div id="wrapper">
+
+  <!-- Navigation -->
+
+  <?php include "includes/admin_navigation.php" ?>
+
+  <div id="page-wrapper">
+
+    <div class="container-fluid">
+
+      <!-- Page Heading -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">
+            Welcome to Admin
+            <small>Author</small>
+          </h1>
+
+          <table class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Author</th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Status</th>
+                <th>Image</th>
+                <th>Tags</th>
+                <th>Comments</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Sumesh Majhi</td>
+                <td>Bootstrap</td>
+                <td>UI Framework</td>
+                <td>Public</td>
+                <td>Image</td>
+                <td>Tags</td>
+                <td>Comment</td>
+                <td>Date</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
+
+  </div>
+  <!-- /#page-wrapper -->
+  <?php include "includes/admin_footer.php" ?>
