9a8bc8c8feab2c8b793e5e76e1f8090617637830
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-10 12:58:45 +0530

    feat: Finally Updating Posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -3,7 +3,7 @@ if (isset($_GET['p_id'])) {
     $the_post_id = $_GET['p_id'];
 }
 
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
 $select_posts_by_id = mysqli_query($connection, $query);
 
 while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
@@ -18,6 +18,44 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_comment_count = $row['post_comment_count'];
     $post_date = $row['post_date'];
 }
+
+if (isset($_POST['update_post'])) {
+
+    $post_author = $_POST['author'];
+    $post_title = $_POST['title'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if (empty($post_image)) {
+        $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+        $select_image = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_array($select_image)) {
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_title = '{$post_title}',";
+    $query .= "post_category_id = '{$post_category_id}',";
+    $query .= "post_date = now(),";
+    $query .= "post_author = '{$post_author}',";
+    $query .= "post_status = '{$post_status}',";
+    $query .= "post_tags = '{$post_tags}',";
+    $query .= "post_content = '{$post_content}',";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id}";
+
+    $update_post = mysqli_query($connection, $query);
+
+    confirmQuery($update_post);
+}
 ?>
 
 
@@ -29,7 +67,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <select name="" id="">
+        <select name="post_category" id="">
             <?php
             $query = "SELECT * FROM categories";
             $select_categories = mysqli_query($connection, $query);
@@ -58,6 +96,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+        <input type="file" name="image">
     </div>
 
     <div class="form-group">
@@ -71,7 +110,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+        <input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
     </div>
 
 </form>
