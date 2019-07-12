dce0060b89cb9f520c97b3b45e46736e49628163
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-12 13:15:13 +0530

    feat: Creating the Frond End comment insert query

--- post.php
+++ post.php
@@ -60,7 +60,15 @@
             <!-- Blog Comments -->
             <?php
             if (isset($_POST['create_comment'])) {
-                echo $_POST['comment_author'];
+                $the_post_id = $_GET['p_id'];
+
+                $comment_author = $_POST['comment_author'];
+                $comment_email = $_POST['comment_email'];
+                $comment_content = $_POST['comment_content'];
+
+                $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
+
+                $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())";
             }
             ?>
 
