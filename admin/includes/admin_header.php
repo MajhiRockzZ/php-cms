7338dfd1e329bd9c0828423652c385642a358f06
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-13 22:43:31 +0530

    feat: Validating User Admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -3,6 +3,14 @@
 <?php ob_start(); ?>
 <?php session_start(); ?>
 
+<?php
+if (isset($_SESSION['user_role'])) {
+    if ($_SESSION['user_role'] !== 'admin') {
+        header("Location: ../index.php");
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
