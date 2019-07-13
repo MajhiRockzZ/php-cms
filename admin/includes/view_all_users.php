0a6b600b04eaa47ff0c0dfd4897ecca90a752967
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-13 11:02:01 +0530

    feat: Creating the display query for users

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -13,17 +13,18 @@
     <tbody>
 
     <?php
-    $query = "SELECT * FROM comments";
-    $select_comments = mysqli_query($connection, $query);
+    $query = "SELECT * FROM users";
+    $select_users = mysqli_query($connection, $query);
 
     while ($row = mysqli_fetch_assoc($select_comments)) {
-        $comment_id = $row['comment_id'];
-        $comment_post_id = $row['comment_post_id'];
-        $comment_author = $row['comment_author'];
-        $comment_content = $row['comment_content'];
-        $comment_email = $row['comment_email'];
-        $comment_status = $row['comment_status'];
-        $comment_date = $row['comment_date'];
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_image = $row['user_image'];
+        $user_role = $row['user_role'];
 
         echo "<tr>";
         echo "<td>$comment_id</td>";
