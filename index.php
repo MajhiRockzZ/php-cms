90d17755008e58f0777983ef7f45ba9d552cc786
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-13 08:40:11 +0530

    feat: Adjustments for comments and Displaying Post Based on Status

--- index.php
+++ index.php
@@ -23,38 +23,45 @@
                 $post_date = $row['post_date'];
                 $post_image = $row['post_image'];
                 $post_content = substr($row['post_content'], 0, 100);
-                ?>
+                $post_status = $row['post_status'];
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                if ($post_status !== 'published') {
+                    echo "<h1 class='text-center'>NO POST SORRY</h1>";
+                } else {
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id ?>">
-                        <?php echo $post_title ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">
-                        <?php echo $post_author ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span>
-                    <?php echo $post_date ?>
-                </p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p>
-                    <?php echo $post_content ?>
-                </p>
-                <a class="btn btn-primary" href="#">Read More <span
-                            class="glyphicon glyphicon-chevron-right"></span></a>
+                    ?>
 
-                <hr>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="post.php?p_id=<?php echo $post_id ?>">
+                            <?php echo $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php">
+                            <?php echo $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span>
+                        <?php echo $post_date ?>
+                    </p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                    <hr>
+                    <p>
+                        <?php echo $post_content ?>
+                    </p>
+                    <a class="btn btn-primary" href="#">Read More <span
+                                class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
 
 
-            <?php } ?>
+                <?php }
+            } ?>
         </div>
 
         <!-- Blog Sidebar Widgets Column -->
