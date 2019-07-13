4bfbba9f27806fc6cb446491779382ccb3e4ab4a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-13 21:43:06 +0530

    feat: Select User Query

--- includes/login.php
+++ includes/login.php
@@ -2,7 +2,20 @@
 
 <?php
 if (isset($_POST['login'])) {
-    echo $username = $_POST['username'];
-    echo $password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
+    $select_user_query = mysqli_query($connection, $query);
+    if (!$select_user_query) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+
+    while ($row = mysqli_fetch_array($select_user_query)) {
+        echo $db_id = $row['user_id'];
+    }
 }
 ?>
