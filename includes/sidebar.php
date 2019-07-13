a08db1331c6be8ced2d26e3658371474823aea1a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-13 21:21:04 +0530

    feat: Creating the login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -5,11 +5,28 @@
         <h4>Blog Search</h4>
         <form action="search.php" method="post">
             <div class="input-group">
+                <input name="search" type="text" class="form-control">
                 <span class="input-group-btn">
-                    <input name="search" type="text" class="form-control">
                     <button name="submit" class="btn btn-default" type="submit">
                         <span class="glyphicon glyphicon-search"></span>
                     </button>
+                </span>
+            </div>
+        </form> <!-- Search Form -->
+        <!-- /.input-group -->
+    </div>
+
+    <!-- Login -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter Username">
+            </div>
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">Submit</button>
                 </span>
             </div>
         </form> <!-- Search Form -->
