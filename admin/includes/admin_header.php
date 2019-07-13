aa591c994d16b560ba5dad894a0fdd6d0e3923fe
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-13 22:30:40 +0530

    feat: Setting Values with Sessions

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,7 +1,7 @@
 <?php include "../includes/db.php"; ?>
 <?php include "functions.php"; ?>
-
 <?php ob_start(); ?>
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html lang="en">
--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,7 @@
 
                     <h1 class="page-header">
                         Welcome to Admin
-                        <small>Author</small>
+                        <small><?php echo $_SESSION['username'] ?></small>
                     </h1>
 
 
