3006a92e7eab6841a7d3e37ef07045a38ea4aac6
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-19 21:25:23 +0530

    feat: Updating User Data

--- admin/profile.php
+++ admin/profile.php
@@ -3,7 +3,9 @@
 <?php
 if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
+
     $query = "SELECT * FROM  users WHERE username = '{$username}'";
+
     $select_user_profile_query = mysqli_query($connection, $query);
 
     while ($row = mysqli_fetch_array($select_user_profile_query)) {
@@ -17,6 +19,29 @@ if (isset($_SESSION['username'])) {
         $user_role = $row['user_role'];
     }
 
+}
+?>
+
+<?php
+if (isset($_POST['edit_user'])) {
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+
+    $query = "UPDATE users SET ";
+    $query .= "user_firstname = '{$user_firstname}', ";
+    $query .= "user_lastname = '{$user_lastname}', ";
+    $query .= "user_role = '{$user_role}', ";
+    $query .= "username = '{$username}', ";
+    $query .= "user_email = '{$user_email}', ";
+    $query .= "user_password = '{$user_password}' ";
+    $query .= "WHERE username = '{$username}' ";
+
+    $edit_user_query = mysqli_query($connection, $query);
+    confirmQuery($edit_user_query);
 }
 ?>
 
