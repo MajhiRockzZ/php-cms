3a85206f79f0883be98f494a6ed5e20eed86ecaa
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-21 21:18:31 +0530

    feat: Cleaning up the chart a little

--- admin/index.php
+++ admin/index.php
@@ -149,17 +149,19 @@
 
                     function drawChart() {
                         var data = google.visualization.arrayToDataTable([
-                            ['Year', 'Sales', 'Expenses', 'Profit'],
-                            ['2014', 1000, 400, 200],
-                            ['2015', 1170, 460, 250],
-                            ['2016', 660, 1120, 300],
-                            ['2017', 1030, 540, 350]
+                            ['Data', 'Count'],
+                            <?php
+
+
+
+                            ?>
+                            ['Post', 1000],
                         ]);
 
                         var options = {
                             chart: {
-                                title: 'Company Performance',
-                                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                                title: '',
+                                subtitle: '',
                             }
                         };
 
@@ -169,7 +171,7 @@
                     }
                 </script>
 
-                <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+                <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
             </div>
 
         </div>
