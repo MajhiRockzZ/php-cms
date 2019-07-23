2ec6e3cf0c18438ba87bd72b61b0ad91dd61cffc
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-23 14:31:27 +0530

    feat: Adding Notification to Add User Page

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -21,6 +21,8 @@ if (isset($_POST['create_user'])) {
     $create_user_query = mysqli_query($connection, $query);
 
     confirmQuery($create_user_query);
+
+    echo "User Created: " . " " . "<a href='users.php'>View Users</a>";
 }
 ?>
 
