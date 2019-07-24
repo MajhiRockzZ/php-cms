9c447c1e47429ce3c334ad61d47c47344502d3a3
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 19:28:13 +0530

    feat: Adding a LOADER to the CMS Admin

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -21,4 +21,12 @@ $(document).ready(function () {
             });
         }
     });
+
+    var div_box = "<div id='load-screen'><div id='loading'></div></div>";
+
+    $("body").prepend(div_box);
+
+    $('#load-screen').delay(700).fadeOut(600, function () {
+        $(this).remove();
+    })
 });
