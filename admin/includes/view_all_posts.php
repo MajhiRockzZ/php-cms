d1a7a8c6f221ff59f087389c86e1c317b4b7e73c
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 18:32:58 +0530

    feat: Cloning Posts NEW feature - COOL STUFF

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -22,6 +22,30 @@ if (isset($_POST['checkBoxArray'])) {
                 confirmQuery($update_to_delete_status);
                 break;
 
+            case 'clone':
+                $query = "SELECT * FROM posts WHERE post_id = '{$postValueId}'";
+                $select_post_query = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_array($select_post_query)) {
+                    $post_title = $row['post_title'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_date = $row['post_date'];
+                    $post_author = $row['post_author'];
+                    $post_status = $row['post_status'];
+                    $post_image = $row['post_image'];
+                    $post_tags = $row['post_tags'];
+                    $post_content = $row['post_content'];
+                }
+                $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status)";
+
+                $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}')";
+
+                $copy_query = mysqli_query($connection, $query);
+
+                if (!$copy_query) {
+                    die("QUERY FAILED" . mysqli_error($connection));
+                }
+                break;
         }
     }
 }
@@ -36,6 +60,7 @@ if (isset($_POST['checkBoxArray'])) {
                     <option value="published">Publish</option>
                     <option value="draft">Draft</option>
                     <option value="delete">Delete</option>
+                    <option value="clone">Clone</option>
                 </select>
             </div>
 
@@ -64,7 +89,7 @@ if (isset($_POST['checkBoxArray'])) {
             <tbody>
 
             <?php
-            $query = "SELECT * FROM posts";
+            $query = "SELECT * FROM posts ORDER BY post_id DESC";
             $select_posts = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc($select_posts)) {
