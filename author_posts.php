b6f16188a3e37e33d86d875f104060eb99684d42
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 17:49:46 +0530

    feat: Relating Posts to their Authors Part 2

--- author_posts.php
+++ author_posts.php
@@ -15,9 +15,10 @@
             <?php
             if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
+                $the_post_author = $_GET['author'];
             }
 
-            $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+            $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}'";
             $select_all_posts_query = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
@@ -38,8 +39,7 @@
                         <?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="#">
-                        <?php echo $post_author ?></a>
+                    All post by <?php echo $post_author ?>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span>
                     <?php echo $post_date ?>
@@ -52,89 +52,6 @@
                 </p>
                 <hr>
 
-
-            <?php } ?>
-
-            <!-- Blog Comments -->
-            <?php
-            if (isset($_POST['create_comment'])) {
-
-                $the_post_id = $_GET['p_id'];
-
-                $comment_author = $_POST['comment_author'];
-                $comment_email = $_POST['comment_email'];
-                $comment_content = $_POST['comment_content'];
-
-                if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
-                    $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
-
-                    $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())";
-
-                    $create_comment_query = mysqli_query($connection, $query);
-                    if (!$create_comment_query) {
-                        die('QUERY FAILED' . mysqli_error($connection));
-                    }
-
-                    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-                    $query .= "WHERE post_id = $the_post_id ";
-                    $update_comment_count = mysqli_query($connection, $query);
-                } else {
-                    echo "<script>alert('Fields cannot be empty')</script>";
-                }
-            }
-            ?>
-
-            <!-- Comments Form -->
-            <div class="well">
-                <h4>Leave a Comment:</h4>
-                <form action="" method="post" role="form">
-
-                    <div class="form-group">
-                        <label for="Author">Author</label>
-                        <input type="text" class="form-control" name="comment_author">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" name="comment_email">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="comment">Your Comment</label>
-                        <textarea name="comment_content" class="form-control" rows="3"></textarea>
-                    </div>
-                    <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-
-            <hr>
-
-            <!-- Posted Comments -->
-            <?php
-            $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
-            $query .= "AND comment_status = 'approved' ";
-            $query .= "ORDER BY comment_id DESC ";
-            $select_comment_query = mysqli_query($connection, $query);
-            if (!$select_comment_query) {
-                die('Query Failed' . mysqli_error($connection));
-            }
-            while ($row = mysqli_fetch_array($select_comment_query)) {
-                $comment_date = $row['comment_date'];
-                $comment_content = $row['comment_content'];
-                $comment_author = $row['comment_author'];
-                ?>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading"><?php echo $comment_author; ?>
-                            <small><?php echo $comment_date; ?></small>
-                        </h4>
-                        <?php echo $comment_content; ?>
-                    </div>
-                </div>
 
             <?php } ?>
 
