31535b221cf5f2cc12c4db04e76c2b288f7b1c49
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 21:15:23 +0530

    feat: Pagination Intro and Part 2 - Links and Get Request

--- index.php
+++ index.php
@@ -18,6 +18,9 @@
             $find_count = mysqli_query($connection, $post_query_count);
             $count = mysqli_num_rows($find_count);
 
+            $count = ceil($count / 5);
+
+
             $query = "SELECT * FROM posts";
             $select_all_posts_query = mysqli_query($connection, $query);
 
@@ -40,6 +43,9 @@
                     </h1>
 
                     <!-- First Blog Post -->
+
+                    <h1><?php echo $count; ?></h1>
+
                     <h2>
                         <a href="post.php?p_id=<?php echo $post_id ?>">
                             <?php echo $post_title ?></a>
@@ -76,5 +82,13 @@
     <!-- /.row -->
 
     <hr>
+
+    <ul class="pager">
+        <?php
+        for ($i = 1; $i <= $count; $i++) {
+            echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+        }
+        ?>
+    </ul>
 
 <?php include "includes/footer.php" ?>
