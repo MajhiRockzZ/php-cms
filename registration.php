6fe2509712281a46608f817e79568e1e58e8a01c
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 13:45:35 +0530

    feat: Starting Query and Default Tables Values

--- registration.php
+++ registration.php
@@ -10,6 +10,13 @@ if (isset($_POST['submit'])) {
     echo $username = mysqli_real_escape_string($connection, $username);
     $email = mysqli_real_escape_string($connection, $email);
     $password = mysqli_real_escape_string($connection, $password);
+
+    $query = "SELECT randSalt FROM users";
+    $select_randsalt_query = mysqli_query($connection, $query);
+
+    if (!$select_randsalt_query) {
+        die("Query Failed" . mysqli_error($connection));
+    }
 }
 ?>
 
