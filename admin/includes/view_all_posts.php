d463ea615a7ed6321e85a416d9e36c1b59764690
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 20:45:01 +0530

    feat: Resetting Views Feature

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -135,7 +135,7 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                 echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
-                echo "<td>{$post_views_count}</td>";
+                echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                 echo "</tr>";
             }
             ?>
@@ -148,6 +148,12 @@ if (isset($_GET['delete'])) {
     $the_post_id = $_GET['delete'];
     $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
     $delete_query = mysqli_query($connection, $query);
+    header("Location: posts.php");
+}
+
+if (isset($_GET['reset'])) {
+    $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
+    $reset_query = mysqli_query($connection, $query);
     header("Location: posts.php");
 }
 ?>
