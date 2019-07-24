1409709a5253e6e383ac20ea6095ec07fbac474a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 14:53:02 +0530

    feat: User Page Display Shorter Password Field

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -32,13 +32,23 @@ if (isset($_POST['edit_user'])) {
 
 //    move_uploaded_file($post_image_temp, "../images/$post_image");
 
+    $query = "SELECT randSalt FROM users";
+    $select_randsalt_query = mysqli_query($connection, $query);
+    if (!$select_randsalt_query) {
+        die("Query Failed" . mysqli_error($connection));
+    }
+
+    $row = mysqli_fetch_array($select_randsalt_query);
+    $salt = $row['randSalt'];
+    $hashed_password = crypt($user_password, $salt);
+
     $query = "UPDATE users SET ";
     $query .= "user_firstname = '{$user_firstname}', ";
     $query .= "user_lastname = '{$user_lastname}', ";
     $query .= "user_role = '{$user_role}', ";
     $query .= "username = '{$username}', ";
     $query .= "user_email = '{$user_email}', ";
-    $query .= "user_password = '{$user_password}' ";
+    $query .= "user_password = '{$hashed_password}' ";
     $query .= "WHERE user_id = {$the_user_id} ";
 
     $edit_user_query = mysqli_query($connection, $query);
