c453b32b39bbe2b4e1e965b92f44fd43b24fd85a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 14:20:48 +0530

    feat: Encrypting User Passwords

--- registration.php
+++ registration.php
@@ -25,6 +25,8 @@ if (isset($_POST['submit'])) {
 
         $salt = $row['randSalt'];
 
+        $password = crypt($password, $salt);
+
         $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
         $query .= "VALUES('{$username}','{$email}','{$password}','subscriber')";
         $register_user_query = mysqli_query($connection, $query);
