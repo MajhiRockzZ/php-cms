8b17a78bb79f54923e3e14915ed22a0e124f1341
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-24 14:36:42 +0530

    feat: Updating Our CMS due to password Encryption

--- includes/login.php
+++ includes/login.php
@@ -24,6 +24,8 @@ if (isset($_POST['login'])) {
         $db_user_role = $row['user_role'];
     }
 
+    $password = crypt($password, $db_user_password);
+
     if ($username === $db_username && $password === $db_user_password) {
         $_SESSION['username'] = $db_username;
         $_SESSION['firstname'] = $db_user_firstname;
