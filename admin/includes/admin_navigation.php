e0d1aed838b0eac534d7067e30a14a2e41e56588
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-07-29 15:39:54 +0530

    feat: Displaying Users Online in Navigation

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -12,6 +12,7 @@
 
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
+        <li><a href="">Users Online: <?php echo users_online(); ?></a></li>
         <li><a href="../index.php">HOME SITE</a></li>
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
