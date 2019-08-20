4cea34d461cc424cdb1be44382265f5f7b200929
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-08-20 16:44:30 +0530

    feat: Adding New System to Add Users in Admin

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -4,15 +4,17 @@ if (isset($_POST['create_user'])) {
     $user_lastname = $_POST['user_lastname'];
     $user_role = $_POST['user_role'];
 
-//    $post_image = $_FILES['image']['name'];
-//    $post_image_temp = $_FILES['image']['tmp_name'];
+    //    $post_image = $_FILES['image']['name'];
+    //    $post_image_temp = $_FILES['image']['tmp_name'];
 
     $username = $_POST['username'];
     $user_email = $_POST['user_email'];
     $user_password = $_POST['user_password'];
-//    $post_date = date('d-m-y');
+    //    $post_date = date('d-m-y');
 
-//    move_uploaded_file($post_image_temp, "../images/$post_image");
+    //    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $user_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 10));
 
     $query = "INSERT INTO users(user_firstname, user_lastname, user_role, username, user_email, user_password)";
 
