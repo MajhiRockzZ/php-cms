5aaa91aed3d1a6c8f494a7d5349b7ab2badf98b8
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-08-20 17:34:28 +0530

    feat: Adding new password system to edit user page part 2

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -34,19 +34,24 @@ if (isset($_POST['edit_user'])) {
         $row = mysqli_fetch_array($get_user_query);
 
         $db_user_password = $row['user_password'];
+
+        if ($db_user_password != $user_password) {
+            $hashed_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 10));
+        }
+
+        $query = "UPDATE users SET ";
+        $query .= "user_firstname = '{$user_firstname}', ";
+        $query .= "user_lastname = '{$user_lastname}', ";
+        $query .= "user_role = '{$user_role}', ";
+        $query .= "username = '{$username}', ";
+        $query .= "user_email = '{$user_email}', ";
+        $query .= "user_password = '{$hashed_password}' ";
+        $query .= "WHERE user_id = {$the_user_id} ";
+
+        $edit_user_query = mysqli_query($connection, $query);
+        confirmQuery($edit_user_query);
+        echo "User Updated" . " <a href='users.php'>View Users?</a>";
     }
-
-    $query = "UPDATE users SET ";
-    $query .= "user_firstname = '{$user_firstname}', ";
-    $query .= "user_lastname = '{$user_lastname}', ";
-    $query .= "user_role = '{$user_role}', ";
-    $query .= "username = '{$username}', ";
-    $query .= "user_email = '{$user_email}', ";
-    $query .= "user_password = '{$hashed_password}' ";
-    $query .= "WHERE user_id = {$the_user_id} ";
-
-    $edit_user_query = mysqli_query($connection, $query);
-    confirmQuery($edit_user_query);
 }
 ?>
 
