95d1aadd09f844ff6d6c7de8cc487cd6df633dfb
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-08-21 12:44:56 +0530

    feat: WARNING BEFORE GOING ONLINE - Edwin from the future

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($_POST['create_post'])) {
-    $post_title = $_POST['title'];
+
+    $post_title = escape($_POST['title']);
     $post_user = $_POST['post_user'];
     $post_category_id = $_POST['post_category'];
     // $post_status = $_POST['post_status'];
