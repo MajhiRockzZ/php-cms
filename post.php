aaebf9c40fcde1aef9a56650566341888aa9bdaa
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-08-21 12:25:29 +0530

    fix: Author not displayed

--- post.php
+++ post.php
@@ -29,7 +29,7 @@
 
                 while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                     $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
+                    $post_author = $row['post_user'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = $row['post_content'] ?>
